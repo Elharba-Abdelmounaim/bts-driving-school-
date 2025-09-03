73454ceff9732a7d20fc0b4c8279c60b777a6bb8
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-09-03 19:25:52 +0100

    fiche views permission_classes

--- bts-driving-school/core/views.py
+++ bts-driving-school/core/views.py
@@ -1,8 +1,38 @@
-# core/views.py
 from rest_framework import viewsets
-from .models import Student
-from .serializers import StudentSerializer
+from rest_framework.permissions import IsAuthenticated
+from .models import Student, Instructor, Vehicle, Wallet, LicenseCategory
+from .serializers import (
+    StudentSerializer, InstructorSerializer, VehicleSerializer,
+    WalletSerializer, LicenseCategorySerializer
+    )
+from .permissions import IsAdmin, IsAdminOrReadOnly
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+
+class InstructorViewSet(viewsets.ModelViewSet):
+    queryset = Instructor.objects.all()
+    serializer_class = InstructorSerializer
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+
+
+class VehicleViewSet(viewsets.ModelViewSet):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+
+
+class WalletViewSet(viewsets.ModelViewSet):
+    queryset = Wallet.objects.all()
+    serializer_class = WalletSerializer
+    permission_classes = [IsAuthenticated, IsAdmin]
+
+
+class LicenseCategoryViewSet(viewsets.ModelViewSet):
+    queryset = LicenseCategory.objects.all()
+    serializer_class = LicenseCategorySerializer
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+
+
