462ea95b5e29bbe294c0f7a0df1ffd6774030b88
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-09-03 19:42:15 +0100

    fiche urls 'JWT' 'Router'

--- bts-driving-school/core/urls.py
+++ bts-driving-school/core/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StudentViewSet
+from .views import StudentViewSet, InstructorViewSet, VehicleViewSet, WalletViewSet, LicenseCategoryViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 router = DefaultRouter()
 router.register(r'students', StudentViewSet)
+router.register(r'instructors', InstructorViewSet)
+router.register(r'vehicles', VehicleViewSet)
+router.register(r'wallets', WalletViewSet)
+router.register(r'license-categories', LicenseCategoryViewSet)
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
