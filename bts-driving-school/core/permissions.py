abc24b7e65e332d2717be17f2dd01be2828320b0
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-09-03 19:53:51 +0100

    fiche permission

--- bts-driving-school/core/permissions.py
+++ bts-driving-school/core/permissions.py
@@ -1,20 +1,20 @@
 from rest_framework import permissions
 
 class IsAdmin(permissions.BasePermission):
-    def has_premission(self, request, view):
-        return request.user.role == 'admin'
+    def has_permission(self, request, view):
+        return request.user.is_autheticated and  request.user.role == 'admin'
 
 class IsInstructor(permissions.BasePermission):
-    def has_premission(self, request, view):
-        return request.user.role == 'instructor'
+    def has_permission(self, request, view):
+        return request.user.is_autheticated and request.user.role == 'instructor'
 
 class IsStudent(permissions.BasePermission):
-    def has_premission(self, request, view):
-        return request.user.role == 'student'
+    def has_permission(self, request, view):
+        return request.user.is_autheticated and request.user.role == 'student'
 
 class IsAdminOrReadOnly(permissions.BasePermission):
-    def has_premission(self, request, view):
+    def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
-            return True
-        return request.user.role == 'admin'
+            return True # GET 
+        return request.user.is_autheticated and request.user.role == 'admin'
         
